ec5ab2efaf261d4e87ffb5f0298c192fc7695447
Almir2021 <bosnjicalmir@gmail.com>
2021-09-22 18:46:41 +0200

    Vjezba 1

---
+++ ex01/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+  
+var myPet = {
+    species: "dober",
+    name : "Rex",
+    legs : 4 ,
+    friends : ["Dino", "Riad"]
+  
+};
+ function myFunction (myObj){
+   return myObj;
+
+ }
+ 
+
+// Only change code above this line
+console.log (myFunction(myPet));
+module.exports = {myPet, myFunction};
