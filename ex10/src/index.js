a9e0ea6f52888bf271fa7cf18116455009f6d1c7
Almir2021 <bosnjicalmir@gmail.com>
2021-09-23 02:19:28 +0200

    Vjezba 10

---
+++ ex10/src/index.js
@@ -0,0 +1,35 @@
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist : "Billy Joel",
+            title : "Piano Man",
+            release_year: 1973,
+            formats : {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+
+            },
+
+
+            gold:true
+
+
+        },
+
+            002: {
+                artist : "Villy Joel",
+                title : "Riano Man",
+                release_year: 1974,
+                formats : {
+                    1: "VD",
+                    2: "9T",
+                    3: "KP"
+    
+                },
+            }
+
+    }     
+}
+myFunction()[2];
+module.exports = myFunction;
